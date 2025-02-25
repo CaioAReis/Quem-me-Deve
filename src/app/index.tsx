56523b90eb770587ba0d8ff864ea7601f225d65d
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-02-24 21:26:14 -0300

    feat: Enhance welcome page with linear gradient and updated layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,47 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { Stack } from "expo-router";
 import { Image } from "react-native";
 
-import { Center, Container } from "@/components/layout";
+import { Button } from "@/components";
+import { Center, Container, Text } from "@/components/layout";
 
 export default function Welcome() {
   return (
     <>
       <Stack.Screen options={{ title: "Welcome" }} />
 
-      <Container className="border border-red-600">
-        <Image className="m-8 size-16" source={require("@/assets/images/logo.png")} />
+      <Container>
+        <Image className="m-8 size-12" source={require("@/assets/images/logo.png")} />
 
-        <Center className="h-96 w-11/12 self-end rounded-l-[80] bg-purple-500">
-          <Image
-            resizeMode="contain"
-            className="ml-9 size-72"
-            source={require("@/assets/images/finances.png")}
-          />
+        <Center className="my-6 h-80 w-10/12 self-end overflow-hidden rounded-l-[80]">
+          <LinearGradient
+            style={{
+              width: "100%",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            end={{ x: 1, y: 0.6 }}
+            colors={["#7740FE", "#2CDC5F"]}
+          >
+            <Image
+              resizeMode="contain"
+              className="ml-9 size-64"
+              source={require("@/assets/images/finances.png")}
+            />
+          </LinearGradient>
+        </Center>
+
+        <Center className="gap-6 p-6">
+          <Text variant="H3" className="text-center leading-[1.6]">
+            {"Garanta o controle do\n seu dinheiro emprestado!"}
+          </Text>
+
+          <Text className="text-center text-base/snug opacity-55">
+            Evite esquecimentos e dores de cabeça mantenha suas finanças no controle.
+          </Text>
+
+          <Button className="mt-8 w-full">Continuar</Button>
         </Center>
       </Container>
     </>
