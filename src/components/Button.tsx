e4d802294f310062cc01efff627282bbc354c5c1
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-02-24 21:26:03 -0300

    feat: Add Button component with linear gradient background

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { ReactNode } from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import { Text } from "./layout";
+
+type Props = {
+  className?: string;
+  children: ReactNode;
+} & TouchableOpacityProps;
+
+export function Button({ children, className, ...rest }: Props) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className={`overflow-hidden rounded-full ${className}`}
+      {...rest}
+    >
+      <LinearGradient end={{ x: 0.6, y: 2 }} colors={["#7740FE", "#2CDC5F"]}>
+        <Text className="m-3 text-center font-poppinsBold color-white">{children}</Text>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+}
