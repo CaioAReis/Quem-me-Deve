2b2995e9626226c9d5ce31a274861d190bc1420f
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-04 15:29:40 -0300

    feat: Enhance Button component with variant support and layout adjustments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,25 +2,38 @@ import { LinearGradient } from "expo-linear-gradient";
 import { ReactNode } from "react";
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
-import { Text } from "./layout";
+import { Center, Text } from "./layout";
 
 type Props = {
   className?: string;
   children: ReactNode;
+  variant?: "solid" | "outline";
 } & TouchableOpacityProps;
 
-export function Button({ children, className, ...rest }: Props) {
+export function Button({ children, className, variant, ...rest }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       className={`overflow-hidden rounded-full ${className}`}
       {...rest}
     >
-      <LinearGradient end={{ x: 0.6, y: 2 }} colors={["#7740FE", "#2CDC5F"]}>
-        <Text variant="H4" className="m-3 text-center color-white">
-          {children}
-        </Text>
+      <LinearGradient style={{ padding: 2 }} end={{ x: 0.6, y: 2 }} colors={["#7740FE", "#2CDC5F"]}>
+        <Center className={`${styles[variant ?? "solid"]} rounded-full`}>
+          <Text variant="H4" className={`${colorStyles[variant ?? "solid"]} m-3 text-center`}>
+            {children}
+          </Text>
+        </Center>
       </LinearGradient>
     </TouchableOpacity>
   );
 }
+
+const styles = {
+  solid: "bg-transparent",
+  outline: "bg-gray-50",
+};
+
+const colorStyles = {
+  solid: "color-white",
+  outline: "color-primary-500",
+};
