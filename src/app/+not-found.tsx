3eded1c5b741fea54a6db620b64658a8601e3b60
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-09 13:55:43 -0300

    feat: Enhance NotFoundScreen with improved layout and visual elements

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,24 +1,27 @@
 import { Link, Stack } from "expo-router";
-import { Text } from "react-native";
+import { Puzzle } from "lucide-react-native";
 
+import { Button } from "@/components";
+import { Center, Text, VStack } from "@/components/layout";
 import { Container } from "@/components/layout/Container";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
+
       <Container>
-        <Text className={styles.title}>This screen doesn"t exist.</Text>
-        <Link href="/" className={styles.link}>
-          <Text className={styles.linkText}>Go to home screen!</Text>
-        </Link>
+        <Center className="flex-1 gap-12 px-10">
+          <Puzzle size={30} color="#7740FE" fill="#7740FE80" />
+          <VStack className="items-center gap-2">
+            <Text variant="H2">Oops!</Text>
+            <Text>Página não encontrada!</Text>
+          </VStack>
+          <Link href="/" asChild>
+            <Button className="w-full">Voltar ao início</Button>
+          </Link>
+        </Center>
       </Container>
     </>
   );
 }
-
-const styles = {
-  title: `text-xl font-bold`,
-  link: `mt-4 pt-4`,
-  linkText: `text-base text-[#2e78b7]`,
-};
