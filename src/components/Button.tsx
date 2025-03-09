683e76dac80d5fbfe438a4eb35f068926c307ce9
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-08 22:51:05 -0300

    feat: Add loading state to Button component with ActivityIndicator

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,36 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { ReactNode } from "react";
-import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { ActivityIndicator, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { twMerge } from "tailwind-merge";
 
 import { Center, Text } from "./layout";
 
 type Props = {
+  loading?: boolean;
   className?: string;
   children: ReactNode;
   variant?: "solid" | "outline";
 } & TouchableOpacityProps;
 
-export function Button({ children, className, variant, ...rest }: Props) {
+export function Button({ children, loading = false, className, variant, ...rest }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className={twMerge("overflow-hidden rounded-full", className)}
+      className={twMerge("overflow-hidden rounded-full disabled:opacity-40", className)}
       {...rest}
     >
       <LinearGradient style={{ padding: 2 }} end={{ x: 0.6, y: 2 }} colors={["#7740FE", "#2CDC5F"]}>
         <Center className={`${styles[variant ?? "solid"]} rounded-full`}>
-          <Text variant="H5" className={`${colorStyles[variant ?? "solid"]} m-2 text-center`}>
-            {children}
-          </Text>
+          {loading ? (
+            <ActivityIndicator
+              size="small"
+              className={`size-11 ${colorStyles[variant ?? "solid"]}`}
+            />
+          ) : (
+            <Text variant="H5" className={`${colorStyles[variant ?? "solid"]} m-2 text-center`}>
+              {children} AA
+            </Text>
+          )}
         </Center>
       </LinearGradient>
     </TouchableOpacity>
