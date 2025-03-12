d2275fe0b47992ee539c8f842c8a25be494bf851
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-12 19:56:30 -0300

    feat: Add Divider component for improved layout in home screen

---
+++ src/app/(user)/home/components/Divider.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@/components/layout";
+
+export function Divider() {
+  return <Box className="my-1 h-[1] w-full bg-gray-200 p-0" />;
+}
--- src/app/(user)/home/components/index.ts
+++ src/app/(user)/home/components/index.ts
@@ -1,5 +1,6 @@
+import { Divider } from "./Divider";
 import { ListEmpty } from "./ListEmpty";
 import { LoanCard } from "./LoanCard";
 import { TabButton } from "./TabButton";
 
-export { TabButton, ListEmpty, LoanCard };
+export { TabButton, ListEmpty, LoanCard, Divider };
