dfe6c0dc449cd41d3a065e56fc9e63182e36d4e8
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-12 19:56:48 -0300

    fix: Improve text styling in LoanCard component for better readability

--- src/app/(user)/home/components/LoanCard.tsx
+++ src/app/(user)/home/components/LoanCard.tsx
@@ -4,11 +4,14 @@ import { Box, HStack, Text, VStack } from "@/components/layout";
 export function LoanCard() {
   return (
     <HStack className="items-center justify-between px-6 py-1">
-      <HStack className="flex-1 gap-3">
+      <HStack className="flex-1 items-center gap-3">
         <Avatar name="Caio AReis" size="xs" />
 
         <VStack className="gap-0">
-          <Text variant="H5">Caio AReis</Text>
+          <Text variant="H5" className="text-gray-800">
+            Caio AReis
+          </Text>
+
           <Text className="text-xs text-gray-600">Desde 20/02/2025</Text>
         </VStack>
       </HStack>
@@ -18,7 +21,9 @@ export function LoanCard() {
           <Text className="font-poppinsMedium text-xs text-white">20%</Text>
         </Box>
 
-        <Text variant="H6">R$ 300,00</Text>
+        <Text variant="H6" className="text-gray-800">
+          R$ 300,00
+        </Text>
       </HStack>
     </HStack>
   );
