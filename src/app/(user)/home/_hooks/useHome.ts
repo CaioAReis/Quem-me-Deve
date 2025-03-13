35c464a5f7147d167cb36bf21b81b3eb63e34b0d
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-13 16:05:37 -0300

    feat: Update useHome hook to manage loan data and calculate loan balance

--- src/app/(user)/home/_hooks/useHome.ts
+++ src/app/(user)/home/_hooks/useHome.ts
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 
+import { Loan } from "@/@types";
+import { loan1, loan2, loan3, loan4, loan5 } from "@/data/loans";
+
 export function useHome({ currentTab }: { currentTab: "pending" | "paid" }) {
-  const [loanList, setLoanList] = useState([]);
+  const [loanList, setLoanList] = useState<Loan[]>([]);
+
+  const loans = [loan1, loan2, loan3, loan4, loan5] as Loan[];
 
   useEffect(() => {
-    setLoanList([]);
+    setLoanList(loans);
   }, []);
 
-  const loanBalance = 2312;
+  const loanBalance =
+    loans.reduce((total, current) => {
+      total += current.totalDebt;
+
+      return total;
+    }, 0) / 100;
 
   return { loanList, loanBalance };
 }
