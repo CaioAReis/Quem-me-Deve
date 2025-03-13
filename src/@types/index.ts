abde436483e7ddc2783f110b92d2d9e534fc5b26
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-13 11:41:59 -0300

    feat: Define User and Loan types for improved type safety and data structure

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -8,3 +8,26 @@ export type TextProps = {
   children: React.ReactNode;
   variant?: "H1" | "H2" | "H3" | "H4" | "H5" | "H6";
 };
+
+export type User = {
+  avatar?: string;
+  name: string;
+  phone: string;
+};
+
+type historyItem = {
+  value: number;
+  createdAt: Date;
+  type: "payment" | "loan";
+};
+
+export type Loan = {
+  user: User;
+  totalDebt: number;
+  history: historyItem[];
+
+  createdAt: Date;
+  updatedAt: Date;
+
+  deadline?: Date;
+};
