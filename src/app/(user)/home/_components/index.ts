5e2cbdd9481c83de37bb7c1eee9059e9493bb861
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-03-13 10:22:09 -0300

    feat: Add new components for improved home screen layout and functionality

--- src/app/(user)/home/_components/index.ts
+++ src/app/(user)/home/_components/index.ts
@@ -1,6 +1,7 @@
 import { Divider } from "./Divider";
+import { Fab } from "./Fab";
 import { ListEmpty } from "./ListEmpty";
 import { LoanCard } from "./LoanCard";
 import { TabButton } from "./TabButton";
 
-export { TabButton, ListEmpty, LoanCard, Divider };
+export { TabButton, ListEmpty, LoanCard, Divider, Fab };
