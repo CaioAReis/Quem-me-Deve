c393b9aec0c88e59bf997e9cf530c53c34152f53
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-04-24 15:28:26 -0300

    feat: Add NewLoanHeader component for improved navigation and layout

---
+++ src/app/(user)/newLoan/_components/NewLoanHeader.tsx
@@ -0,0 +1,22 @@
+import { ArrowLeftCircle } from "lucide-react-native";
+import { TouchableOpacity } from "react-native";
+
+import { HStack, Text } from "@/components/layout";
+
+type Props = {
+  color: string;
+  title?: string;
+  handleBack: VoidFunction;
+};
+
+export function NewLoanHeader({ handleBack, color, title }: Props) {
+  return (
+    <HStack className="m-4 items-center ">
+      <TouchableOpacity onPress={handleBack}>
+        <ArrowLeftCircle size={30} color={color} />
+      </TouchableOpacity>
+
+      <Text variant="H5">{title ?? ""}</Text>
+    </HStack>
+  );
+}
