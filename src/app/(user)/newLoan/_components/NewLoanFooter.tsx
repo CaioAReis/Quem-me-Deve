b424fee3d519e03881136acbe9dae75e74fb09d6
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-04-24 15:28:34 -0300

    feat: Add NewLoanFooter component for displaying footer content

---
+++ src/app/(user)/newLoan/_components/NewLoanFooter.tsx
@@ -0,0 +1,24 @@
+import { Image } from "react-native";
+
+import { Center, Text } from "@/components/layout";
+
+type Props = {
+  image?: string;
+  title?: string;
+};
+
+export function NewLoanFooter({ image, title }: Props) {
+  return (
+    <Center className="mb-36 mt-12 flex-1 gap-6">
+      <Image
+        className="size-56"
+        resizeMode="contain"
+        source={image ?? require("@/assets/images/burn.png")}
+      />
+
+      <Text variant="H5" className="text-center text-gray-800 dark:text-gray-200">
+        {title ?? ""}
+      </Text>
+    </Center>
+  );
+}
