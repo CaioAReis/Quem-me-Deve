1c1318169d0ae776801e171e46b211ccfbddc07a
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-04-24 15:29:03 -0300

    feat: Refactor NewLoan component to integrate LoanForm and improve submission handling

---
+++ src/app/(user)/newLoan/_components/index.ts
@@ -0,0 +1,5 @@
+import { LoanForm } from "./LoanForm";
+import { NewLoanFooter } from "./NewLoanFooter";
+import { NewLoanHeader } from "./NewLoanHeader";
+
+export { LoanForm, NewLoanHeader, NewLoanFooter };
