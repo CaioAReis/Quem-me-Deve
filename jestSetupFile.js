a6dd5e09aa02521d5d2ad927b08ee97789545401
CaioAReis <Caio.almeida16.ca@gmail.com>
2025-06-24 18:35:21 -0300

    chore: Enable coverage collection in Jest and remove unused test file for useStartSessionForm

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -161,10 +161,3 @@ jest.mock("expo-font", () => ({
 jest.mock("@/store/session", () => ({
   useSessionStore: jest.fn(),
 }));
-
-jest.mock("expo-router", () => ({
-  router: {
-    push: jest.fn(),
-    goBack: jest.fn(),
-  },
-}));
